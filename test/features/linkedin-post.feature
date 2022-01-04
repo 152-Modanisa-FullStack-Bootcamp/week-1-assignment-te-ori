b158192a440e62473e1b83c6cafa8bfbfaa1a37f
Orhan Aygün <orh.aygun@gmail.com>
2022-01-04 11:11:41 +0300

    linkedin post's tests added

---
+++ test/features/linkedin-post.feature
@@ -0,0 +1,5 @@
+Feature: Display post screen
+    Scenario: To see post screen.
+        Given The browser is open
+        When Open index page
+        Then Check post
---
+++ test/features/steps/linkedin-post.js
@@ -0,0 +1,21 @@
+const {When, Then, Given} = require('cucumber');
+const puppetter = require('puppeteer')
+
+Given('The browser is open', async function(){
+    this.browser = await puppetter.launch({headless: false});
+    this.page = await this.browser.newPage();
+});
+
+When('Open index page', async function(){
+    await this.page.goto('http://localhost:1234');
+});
+
+Then('Check post', async function(){
+    let post_count = await this.page.$$(".linkedin-post").length;
+    await this.page.close();
+    await this.browser.close();
+
+    if (post_count != 2) {
+        throw new Error("Wrong count");
+    }
+});
